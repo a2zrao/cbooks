4bb3395b60d9b1e8de7c238665dc24866ffbe438
cbooks <115538489+a2zrao@users.noreply.github.com>
2022-11-26 12:10:48 +0530

    Update user-check.php

--- user-check.php
+++ user-check.php
@@ -1,4 +1,4 @@
-<?php
+
 
 <?php
 $servername = "database-1.cdpvsu40laxq.ap-south-1.rds.amazonaws.com";
@@ -12,7 +12,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-?>
+
 
 session_start();
 
